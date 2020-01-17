519f007e4cb8a3f740fdc0292204ea0c40bd9b0f
vanek1821 <33785770+vanek1821@users.noreply.github.com>
2020-01-17 19:07:32 +0100

    Test fixed
    Fixed package declaration

---
+++ src/test/test/java/vanek/pia/PiaApplicationTests.java
@@ -0,0 +1,14 @@
+package vanek.pia;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PiaApplicationTests {
+
+	@Test
+	void contextLoads() {
+		// Nothing here, just testing start
+	}
+
+}
---
+++ src/test/test/java/vanek/pia/RoleRepoTest.java
@@ -0,0 +1,47 @@
+package vanek.pia;
+
+import javax.transaction.Transactional;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import lombok.extern.slf4j.Slf4j;
+import vanek.pia.dao.RoleRepository;
+import vanek.pia.domain.Role;
+
+@SpringBootTest
+@Slf4j
+public class RoleRepoTest {
+
+	@Autowired
+	private RoleRepository roleRepo;
+
+	@Test
+	@Transactional
+	public void testRoleManipulation() {
+		log.info("Testing user manipulation.");
+		Assertions.assertEquals(3, roleRepo.count());
+		log.info("Looking for admin role.");
+		Role adminRole = roleRepo.findByCode("ADMIN");
+		Assertions.assertNotNull(adminRole);
+		log.info("Found admin role.");
+		log.info("Looking for user role.");
+		Role userRole = roleRepo.findByCode("USER");
+		Assertions.assertNotNull(userRole);
+		log.info("Found user role.");
+		log.info("Looking for accountant role.");
+		Role accRole = roleRepo.findByCode("ACCOUNTANT");
+		Assertions.assertNotNull(accRole);
+		log.info("Found accountant role.");
+		//Assertions.assertEquals(1, user.getRoles().size());
+		//log.info("Admin has one role, removing.");
+		//user.getRoles().remove(0);
+		//userRepo.save(user);
+		//user = userRepo.findByUsername("admin");
+		//Assertions.assertEquals(0, user.getRoles().size());
+		//log.info("Admin has no role now, OK.");
+	}
+
+}
